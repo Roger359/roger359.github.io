ef5b3d424f284ed351f3454f128d0b90833b21af
Roger Acosta <rogeracosta359@gmail.com>
2023-10-22 18:16:16 -0500

    add project js file + set to portfolio section

---
+++ js/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: "Lersurveyors",
+    // description: "All kind of Beginner projects with JavaScript Vanilla",
+    img: "./img/p1.png",
+    src: "https://lersurveyors.com/0164.netlify.app/",
+    repository: 'https://github.com/Roger359/lersurveyors'
+  },
+  {
+    title: "Cpven Intranet Corporativa",
+    // description: "All kind of Beginner projects with JavaScript Vanilla",
+    img: "./img/cpven-home.PNG",
+    src: "/pages/cpven.html",
+    // repository: 'https://github.com/Roger359/Demos/blob/master/1.Functions_App/script.js'
+  },{
+    title: "Demo Apps",
+    // description: "100 JavaScript Projects",
+    img: "./img/p3.png",
+    src: "https://quiet-dolphin-a10164.netlify.app/",
+    repository: 'https://github.com/Roger359/Demos'
+  }
+  
+]
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { projects } from "./projects.js";
+
 let visibleMenu = false;
 
 // show & hide menu function
@@ -19,7 +21,7 @@ function select() {
 }
 
 // skills bars functions 
-function skillsEfects(){
+function skillsEffects(){
   let skills = document.getElementById('skills');
   let skillsDistance = window.innerHeight - skills.getBoundingClientRect().top;
   if(skillsDistance >= 300){
@@ -34,11 +36,9 @@ function skillsEfects(){
 }
 
 // detect scroll to apply the animations to the skill bar
-
 window.onscroll = function(){
-  skillsEfects();
+  skillsEffects();
 }
-
 
 // International Telephone Input
 
@@ -46,3 +46,73 @@ const input = document.querySelector("#phone");
 window.intlTelInput(input, {
   utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
 });
+
+const grid = document.querySelector('.grid')
+
+for (let i = 0; i < projects.length; i++){
+  console.log(projects[i]);
+
+  const divContainer = document.createElement("div");
+  divContainer.classList.add("project-container");
+
+  const anchorContainer = document.createElement("a");
+  const divInsideContainer = document.createElement("div");
+  divInsideContainer.classList.add("anchor-image");
+  
+
+  const anchorImg = document.createElement("img");
+  anchorImg.classList.add("image");
+  anchorImg.src = projects[i].img;
+  // demoAnchor.href = projects[i].src;
+ 
+
+  divInsideContainer.appendChild(anchorImg);
+  anchorContainer.appendChild(divInsideContainer);
+  anchorContainer.href = projects[i].src
+  anchorContainer.target = '_blank'
+
+  const divTexts = document.createElement("div");
+  divTexts.classList.add("divTexts");
+
+  const anchorTitle = document.createElement("a");
+  anchorTitle.classList.add("anchor-title");
+
+  const h2Title = document.createElement("h2");
+  h2Title.classList.add("h2-title");
+  h2Title.textContent = projects[i].title;
+
+  anchorTitle.appendChild(h2Title);
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = projects[i].description;
+
+  divTexts.appendChild(anchorTitle);
+  divTexts.appendChild(description);
+
+  const demoDiv = document.createElement("div");
+  demoDiv.classList.add("demo");
+  const demoAnchor = document.createElement("a");
+
+  demoAnchor.textContent = "Demo";
+  demoAnchor.target = "_blank";
+  demoAnchor.href = projects[i].src;
+  
+  const demoRepo = document.createElement("a");
+  demoRepo.target = '_blank'
+  demoRepo.href = projects[i].repository;
+  const iconRepo = document.createElement("i");
+  iconRepo.classList.add("fa-brands");
+  iconRepo.classList.add("fa-github");
+
+  demoRepo.appendChild(iconRepo);
+  demoDiv.appendChild(demoAnchor);
+  demoDiv.appendChild(demoRepo);
+
+  divContainer.appendChild(anchorContainer);
+  divContainer.appendChild(divTexts);
+  divContainer.appendChild(demoDiv);
+
+  grid.appendChild(divContainer);
+
+}
